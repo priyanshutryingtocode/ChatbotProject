d07fb3793971b65a1af7d54f258312fe40ec5bb9
Priyanshu Srivastava <priyanshusrivastava406@gmail.com>
2025-09-04 23:18:10 +0530

    fix

--- setup.py
+++ setup.py
@@ -1,27 +1,18 @@
 import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
-<<<<<<< HEAD
 from langchain_google_genai import ChatGoogleGenerativeAI
-=======
-from langchain_openai import ChatOpenAI
 import streamlit as st
->>>>>>> 6df1b21dcd9167a6badfdab7732af999e1a22f26
+
 
 load_dotenv()
 
 # Setup
 
-<<<<<<< HEAD
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-=======
-OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
-SUPABASE_URL = st.secrets['SUPABASE_URL']
-SUPABASE_KEY = st.secrets['SUPABASE_KEY']
->>>>>>> 6df1b21dcd9167a6badfdab7732af999e1a22f26
 # System prompt for the assistant
 SYSTEM_PROMPT = """You are a professional Order Status Assistant. You help customers check their order status using the database information provided.
 
